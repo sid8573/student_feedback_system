8c8416d774c2b187c4a52b3d4d08a533f26102af
Mohd Sadique <74016206+sid8573@users.noreply.github.com>
2021-01-14 00:21:38 +0530

    Add files via upload

---
+++ database/feedback.sql
@@ -0,0 +1,159 @@
+
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `feedback_system`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE IF NOT EXISTS `admin` (
+  `admin_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user` varchar(50) NOT NULL,
+  `pass` varchar(50) NOT NULL,
+  PRIMARY KEY (`admin_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact`
+--
+
+CREATE TABLE IF NOT EXISTS `contact` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `mobile` bigint(20) NOT NULL,
+  `message` text NOT NULL,
+  `Date` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+--
+-- Dumping data for table `contact`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `faculty`
+--
+
+CREATE TABLE IF NOT EXISTS `faculty` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_alias` varchar(30) NOT NULL,
+  `Name` varchar(30) NOT NULL,
+  `designation` varchar(100) NOT NULL,
+  `programme` varchar(50) NOT NULL,
+  `semester` varchar(10) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(75) NOT NULL,
+  `mobile` bigint(20) NOT NULL,
+  `date` datetime NOT NULL,
+  `status` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
+
+--
+-- Dumping data for table `faculty`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedback`
+--
+
+CREATE TABLE IF NOT EXISTS `feedback` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `student_id` varchar(50) NOT NULL,
+  `faculty_id` varchar(50) NOT NULL,
+  `Teacher provided the course outline having weekly content plan w` enum('5','4','3','2','1') NOT NULL,
+  `Course objectives,learning outcomes and grading criteria are cle` enum('5','4','3','2','1') NOT NULL,
+  `Course integrates throretical course concepts with the real worl` enum('5','4','3','2','1') NOT NULL,
+  `Teacher is punctual,arrives on time and leaves on time` enum('5','4','3','2','1') NOT NULL,
+  `Teacher is good at stimulating the interest in the course conten` enum('5','4','3','2','1') NOT NULL,
+  `Teacher is good at explaining the subject matter` enum('5','4','3','2','1') NOT NULL,
+  `Teacher's presentation was clear,loud ad easy to understand` enum('5','4','3','2','1') NOT NULL,
+  `Teacher is good at using innovative teaching methods/ways` enum('5','4','3','2','1') NOT NULL,
+  `Teacher is available and helpful during counseling hours` enum('5','4','3','2','1') NOT NULL,
+  `Teacher has competed the whole course as per course outline` enum('5','4','3','2','1') NOT NULL,
+  `Teacher was always fair and impartial:` enum('5','4','3','2','1') NOT NULL,
+  `Assessments conducted are clearly connected to maximize learinin` enum('5','4','3','2','1') NOT NULL,
+  `What I liked about the course` text NOT NULL,
+  `Why I disliked about the course` text NOT NULL,
+  `date` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=19 ;
+
+--
+-- Dumping data for table `feedback`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `files`
+--
+
+CREATE TABLE IF NOT EXISTS `files` (
+  `notice_id` int(11) NOT NULL AUTO_INCREMENT,
+  `attachment` varchar(255) NOT NULL,
+  `subject` varchar(100) NOT NULL,
+  `Description` text NOT NULL,
+  `Date` datetime NOT NULL,
+  PRIMARY KEY (`notice_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
+
+--
+-- Dumping data for table `files`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` char(40) NOT NULL,
+  `email` varchar(40) NOT NULL,
+  `pass` varchar(40) NOT NULL,
+  `mobile` bigint(11) NOT NULL,
+  `programme` varchar(20) NOT NULL,
+  `semester` varchar(10) NOT NULL,
+  `gender` varchar(40) NOT NULL,
+  `hobbies` varchar(40) NOT NULL,
+  `image` varchar(50) NOT NULL,
+  `dob` date NOT NULL,
+  `regid` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`),
+  FULLTEXT KEY `name` (`name`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15 ;
+
+--
+-- Dumping data for table `user`
+--
+
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
